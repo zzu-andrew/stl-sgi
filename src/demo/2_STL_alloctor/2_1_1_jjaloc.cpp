056ea90a1c9aca2f7208ff55fb95862a7bac5af7
zzu-andrew <564631192@qq.com>
2024-05-16 23:47:27 +0800

    alloctor添加

---
+++ src/demo/2_STL_alloctor/2_1_1_jjaloc.cpp
@@ -0,0 +1,45 @@
+//
+// Created by andrew on 2024/5/16.
+//
+
+#include "2_1_1_jjalloc.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define __ALIGN 8
+
+int round_up(int b) {
+    return (((b) + __ALIGN - 1) & ~(__ALIGN - 1));
+}
+
+int main() {
+    int ia[5] = {0, 1, 2, 3, 4};
+    int i;
+
+    vector<int, JJ::allocator<int> > iv(ia, ia + 5);
+    for (i = 0; i < iv.size(); i++) {
+        cout << iv[i] << ' ';
+    }
+    cout << endl;
+
+    cout << ~(__ALIGN - 1) << endl;
+    cout << round_up(3) << endl;
+    cout << round_up(15) << endl;
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
